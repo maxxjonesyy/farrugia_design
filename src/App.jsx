9301cdfb2be21d0d32ca36d0fa36c72a81cb082e
Maxwell Jones <maxxjonesyy@gmail.com>
2023-06-20 13:55:43 +1000

    add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./styles/sass/app.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Home />
+      {window.location.pathname === "/" ? null : <Navbar />}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
