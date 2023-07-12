87f13b785e9193f68b4865dbf003f9935e70ac21
Maxwell Jones <maxxjonesyy@gmail.com>
2023-07-12 18:46:26 +1000

    add news page with instagram plugin

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,5 +1,18 @@
+import Navbar from "../components/Navbar";
+import "../styles/sass/news.scss";
+
 function News() {
-  return <div>News</div>;
+  return (
+    <div id='news'>
+      <Navbar />
+
+      <iframe
+        className='instagram'
+        scrolling='yes'
+        src='https://embedsocial.com/facebook_album/pro_hashtag/04c7e2e22bdb90ba683fe00598f477caa25ca07f'
+      ></iframe>
+    </div>
+  );
 }
 
 export default News;
