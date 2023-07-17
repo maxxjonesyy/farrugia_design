792e99b373b469d10e7c1641a17e74fbde0dbc27
Maxwell Jones <maxxjonesyy@gmail.com>
2023-07-17 16:42:49 +1000

    add contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,45 @@
+import Navbar from "../components/Navbar";
+import "../styles/sass/contact.scss";
+
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div id='contact'>
+      <Navbar />
+
+      <form
+        className='form'
+        action='https://formsubmit.co/maxxjonesyy@gmail.com'
+        method='POST'
+      >
+        <div className='info'>
+          <div>
+            <h2>Get in touch:</h2>
+            <p>0404 074 038</p>
+            <p>sally@farrugiadesign.com.au</p>
+          </div>
+
+          <div>
+            <h2>Studio:</h2>
+            <p>Sydney // Bellingen</p>
+            <p>(BY APPOINTMENT ONLY)</p>
+          </div>
+        </div>
+
+        <input type='text' name='name' placeholder='Name' required />
+        <input type='email' name='email' placeholder='Email' required />
+        <textarea
+          name='message'
+          cols='30'
+          rows='10'
+          placeholder='Message...'
+          required
+        ></textarea>
+        <button className='submit' type='submit'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
